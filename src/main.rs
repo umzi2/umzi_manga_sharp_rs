6f761209e9d975d169b20f9292f23b16e711f6db
umzi2 <ab5447612@gmail.com>
2024-01-10 19:36:25 +0300

    feat: config read fix: commit median filter

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,58 @@ use std::{fs};
 use image::GrayImage;
 use rayon::prelude::*;
 use ndarray::{Array2};
-use imageproc::filter::median_filter;
+//use imageproc::filter::median_filter;
 
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use std::io::Read;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    low_input: u8,
+    high_input: u8,
+    low_output: u8,
+    high_output: u8,
+    gamma: f32,
+    diapason_black: i16,
+    diapason_white: i16,
+    cenny: u8,
+}
+
+static CONFIG: Lazy<Config> = Lazy::new(|| load_config());
+
+fn load_config() -> Config {
+    // Указываем путь к JSON-файлу
+    let file_path = "config.json";
+
+    // Открываем файл
+    let mut file = match File::open(&file_path) {
+        Ok(file) => file,
+        Err(err) => {
+            panic!("Не удалось открыть файл {}: {}", file_path, err);
+        }
+    };
+
+    // Создаем строку, в которую будем считывать содержимое файла
+    let mut file_content = String::new();
+
+    // Считываем содержимое файла в строку
+    match file.read_to_string(&mut file_content) {
+        Ok(_) => {
+            // Распарсим JSON-строку с использованием serde_json
+            match serde_json::from_str(&file_content) {
+                Ok(config) => config,
+                Err(err) => {
+                    panic!("Не удалось распарсить JSON: {}", err);
+                }
+            }
+        }
+        Err(err) => {
+            panic!("Не удалось считать содержимое файла {}: {}", file_path, err);
+        }
+    }
+}
 
 fn color_levels(mut image: GrayImage, scores: HashMap<&str, u8>, gamma: f32) -> GrayImage {
     let width = image.width() as usize;
@@ -13,8 +63,8 @@ fn color_levels(mut image: GrayImage, scores: HashMap<&str, u8>, gamma: f32) -> 
     let input: Vec<u8> = image.clone().into_raw();
     let mut array = Array2::from_shape_vec((width, height), input).unwrap();
 
-    array.mapv_inplace(|x| if x + scores["in_high"] <= u8::MAX.into() { x + scores["in_high"] } else { u8::MAX });
-    array.mapv_inplace(|x| if x >= scores["in_low"] { x - scores["in_low"] } else { 0 });
+    array.mapv_inplace(|x| x.saturating_add(255 - scores["in_high"]));
+    array.mapv_inplace(|x| x.saturating_sub(scores["in_low"]));
     array.mapv_inplace(|x| {
         let result = (x as f32 * gamma).round() as u8;
         if result > u8::MAX {
@@ -26,8 +76,8 @@ fn color_levels(mut image: GrayImage, scores: HashMap<&str, u8>, gamma: f32) -> 
     let max_value = array.fold(0, |acc, &x| acc.max(x));
     array.mapv_inplace(|x| ((x as f32 / max_value as f32) * 255.0).round() as u8);
 
-    array.mapv_inplace(|x| if x + (255 - scores["out_high"]) <= u8::MAX.into() { x + (255 - scores["out_high"]) } else { u8::MAX });
-    array.mapv_inplace(|x| if x >= scores["out_low"] { x - scores["out_low"] } else { 0 });
+    array.mapv_inplace(|x| x.saturating_add(255 - scores["out_high"]));
+    array.mapv_inplace(|x| x.saturating_sub(scores["out_low"]));
     let input_vec: Vec<u8> = array.into_raw_vec();
     image.copy_from_slice(&input_vec);
     image
@@ -35,16 +85,16 @@ fn color_levels(mut image: GrayImage, scores: HashMap<&str, u8>, gamma: f32) -> 
 
 fn process_images(path2: &str, output_path: &str) {
     let gray_img = image::open(path2).expect("Не удалось загрузить изображение").to_luma8();
-
+    let config = &*CONFIG;
     let gamma: f32 = 1.0;
     let mut scores = HashMap::new();
-    scores.insert("in_high", 0);
-    scores.insert("in_low", 0);
-    scores.insert("out_low", 0);
-    scores.insert("out_high", 255);
+    scores.insert("in_high", config.high_input);
+    scores.insert("in_low", config.low_input);
+    scores.insert("out_low", config.low_output);
+    scores.insert("out_high", config.high_output);
 
     let gray_img = color_levels(gray_img, scores, gamma);
-    let gray_img = median_filter(&gray_img, 2, 2);
+    //let gray_img = median_filter(&gray_img, 2, 2);
 
     let _ = gray_img.save(output_path);
 }
